9cb0f14ec6c563639a65d2424940454d84a00d3b
9702g <91326552+9702g@users.noreply.github.com>
2024-02-07 04:59:39 +0300

    Update modelapp.py

--- modelapp.py
+++ modelapp.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd 
-import sklearn.datasets 
 from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor 
 from sklearn import metrics
